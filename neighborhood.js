23cd38331c62430a834311ed1be09f66ecd1fc6a
Alissa Reynolds <alissa6710@gmail.com>
2023-09-29 16:13:49 -0500

    started extra credit

---
+++ neighborhood.js
@@ -0,0 +1,4 @@
+const resturants = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * resturants.length);
+console.log(random, resturants[random]);
